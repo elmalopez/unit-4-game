b40f2b8a1b9da7e37ff7f9807ce52202d2b48e88
Elma Lopez <elmalopez@Elmas-MacBook-Pro.local>
2018-11-12 19:05:46 -0800

    add crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,31 @@
 var randomResult;
 var lost;
 var win;
+var previous = 0;
+
+
+randomResult = Math.floor(Math.random() * 69 ) + 30;
+
+$("#result").html('Random Result: ' + randomResult);
 
 for(var i = 0; i  < 4; i++){
-    var random = Math.floor(Math.random() * 12);
+    var random = Math.floor(Math.random() * 11) + 1;
     console.log(random);
 
 var crystal = $("<div>");
     crystal.attr({
-        "class": 'crystal', "data-random": random
+        "class": 'crystal', 
+        "dataRandom": random
     }
 );
 $(".crystals").append(crystal); 
 
 }
+$(".crystal").on('click', function() {
+var num = parseInt($(this).attr('dataRandom'));
+previous += num;
+console.log(previous);
+});
 
 // a game with 4 crystals and random result
 // every crystal needs a random number thats between 1-12
