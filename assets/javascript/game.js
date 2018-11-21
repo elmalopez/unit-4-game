1dac3ddc158d24e2ba0dbd80fbcd34319972d8e2
Elma Lopez <elmalopez@dhcp-v118-175.mobile.uci.edu>
2018-11-20 17:35:53 -0800

    crystals seem to be working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,12 @@ var previous = 0;
 
 var resetAndStart = function () {
     $(".crystals").empty();
+    var images = [
+        "assets/images/shutterstock_618617792.jpg",
+        "assets/images/shutterstock_693134929.jpg",
+        "assets/images/shutterstock_700356640.jpg",
+        "assets/images/shutterstock_736064353.jpg",
+    ]
     randomResult = Math.floor(Math.random() * 69) + 30;
 
     $("#result").html('Random Result: ' + randomResult);
@@ -18,39 +24,46 @@ var resetAndStart = function () {
         crystal.attr({
             "class": 'crystal',
             "dataRandom": random
+
         });
-        crystal.html(random);
+        crystal.css({
+            "background-image": "url('" + images[i] + "')",
+            "background-size":"cover"
+        });
+
         $(".crystals").append(crystal);
 
     }
-
-}
-resetAndStart();
-
-
-
-$(document).on('click', ".crystal", function () {
-    var num = parseInt($(this).attr('dataRandom'));
-    previous += num;
-    console.log(previous);
-
-    if (previous > randomResult) {
-        lost--;
-        $("#lost").html(lost);
-        previous = 0;
-
-        resetAndStart();
-    }
-
-    else if (previous === randomResult) {
-        win++;
-        $("#win").html(win);
-        previous = 0;
-        resetAndStart();
+        $("#previous").html("Total score: " + previous);
 
     }
+    resetAndStart();
 
-});
+
+
+    $(document).on('click', ".crystal", function () {
+        var num = parseInt($(this).attr('dataRandom'));
+        previous += num;
+        $("#previous").html("Total score: " + previous);
+        console.log(previous);
+
+        if (previous > randomResult) {
+            lost++;
+            $("#lost").html("You lost: " + lost);
+            previous = 0;
+
+            resetAndStart();
+        }
+
+        else if (previous === randomResult) {
+            win++;
+            $("#win").html("You win: ", win);
+            previous = 0;
+            resetAndStart();
+
+        }
+
+    });
 
 // a game with 4 crystals and random result
 // every crystal needs a random number thats between 1-12
